608c81f9e0d308516f531c5d4a164c938fb66ee1
kunhe2000 <79676602+kunhe2000@users.noreply.github.com>
2022-05-19 22:21:29 +0900

    크롤링2

--- app.py
+++ app.py
@@ -1,52 +1,44 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbhomework
 
-## HTML을 주는 부분
+
+## HTML 화면 보여주기
 @app.route('/')
-def home():
-   return render_template('index.html')
+def homework():
+    return render_template('index.html')
 
-@app.route('/memo', methods=['GET'])
-def listing():
-    articles = list(db.articles.find({}, {'_id': False}))
-    return jsonify({'all_articles':articles})
 
-## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def saving():
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    # 여기에 코딩을 해서 meta tag를 먼저 가져와보겠습니다.
-
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-    desc = soup.select_one('meta[property="og:description"]')['content']
+# 주문하기(POST) API
+@app.route('/order', methods=['POST'])
+def save_order():
+    name_receive = request.form['name_give']
+    count_receive = request.form['count_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
 
     doc = {
-        'title': title,
-        'image': image,
-        'desc': desc,
-        'url' : url_receive,
-        'comment' : comment_receive
+        'name' : name_receive,
+        'count' : count_receive,
+        'address' : address_receive,
+        'phone' : phone_receive
     }
-    db.articles.insert_one(doc)
+    db.orders.insert_one(doc)
+
+    return jsonify({'result':'success', 'msg': '주문완료'})
 
 
-    return jsonify({'msg':'저장이 완료되었습니다!'})
+# 주문 목록보기(Read) API
+@app.route('/order', methods=['GET'])
+def view_orders():
+    orders = list(db.orders.find({}, {'_id':False} ))
+    return jsonify({'result':'success', 'orders':orders})
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
